b06123495c7358b6590554c01ddda86873d189a0
stecoleman <stecoleman1@gmail.com>
2022-02-10 18:50:26 +0000

    base currency to new currency variables added. fetch working with api link.

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+//import node fetch to get the api data
 import fetch from 'node-fetch';
+let settings = { method: "Get" };
 
-const response = await fetch('http://data.fixer.io/api/latest?access_key=0311cb48110274bc5ce834ea3270bb07');
-const data = await response.json();
+//base currency 
+var fromCurrency = 'gbp'; //add toUpperCase()
+//currency to convert into
+var toCurrency = "USD"; //add toUpperCase()
+//amount of base currency to convert
+var fromAmount = 5;
 
-console.log(data);
+//api link with currency variables set 
+var api = 'https://api.getgeoapi.com/v2/currency/convert?api_key=ae69e404bebee939d172f40d10a54f77be475ffa&from=' + fromCurrency + '&to=' + toCurrency
+
+fetch(api, settings)
+.then(res => res.json())
+.then((json) => {
+
+    //get the currency rate from the api and wrap in variable
+    var currencyRate = json.rates[toCurrency].rate;
+
+    //multiply users entered amount by currency rate to 
+    var returnableAmount = Number(currencyRate) * fromAmount;
+    console.log(returnableAmount);
+
+});
