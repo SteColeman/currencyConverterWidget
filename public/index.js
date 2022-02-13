a01d27449e380407c2b1054b2b15682bfa04dd0e
stecoleman <stecoleman1@gmail.com>
2022-02-13 14:50:33 +0000

    single conversion amount added on seperate line. to and from currency full title also added

--- public/index.js
+++ public/index.js
@@ -1,22 +1,26 @@
 //settings for fetch get request
 let settings = { method: "Get" };
-//base currency 
-var fromCurrency = document.getElementById('baseCurrency').value.toUpperCase() //add toUpperCase()
-//currency to convert into
-var toCurrency =  document.getElementById('toCurrency').value.toUpperCase(); //add toUpperCase()
-//amount of base currency to convert
-var fromAmount =  document.getElementById('amount').value;
+//global vars that need resetting each time the api is pinged
+// var fromCurrency;
+// var toCurrency;
+// var fromAmount;
 
-
-document.getElementById('send').addEventListener('click', getConversion())
-// document.getElementById('send').on('click', getConversion())
+document.getElementById('send').addEventListener('click', getConversion)
 
 function getConversion() {
 
+    //base currency 
+    var fromCurrency = document.getElementById('baseCurrency').value.toUpperCase() //add toUpperCase()
+    //currency to convert into
+    var toCurrency =  document.getElementById('toCurrency').value.toUpperCase(); //add toUpperCase()
+    //amount of base currency to convert
+    var fromAmount =  document.getElementById('amount').value;
+
     //api link with currency variables set 
-    var api = 'https://api.getgeoapi.com/v2/currency/convert?api_key=ae69e404bebee939d172f40d10a54f77be475ffa&from=' + fromCurrency + '&to=' + toCurrency
+    var api = 'https://api.getgeoapi.com/v2/currency/convert?api_key=ae69e404bebee939d172f40d10a54f77be475ffa&from=' + fromCurrency + '&to=' + toCurrency;
 
     fetch(api, settings)
+    //response put into json format
     .then(res => res.json())
     .then((json) => {
 
@@ -27,13 +31,20 @@ function getConversion() {
             //if response status doesnt fail, continue with currency conversion
         } else {
         //get the currency rate from the api and wrap in variable
+            console.log(json);
         var currencyRate = json.rates[toCurrency].rate;
+        //base currency full name
+        var fromCurrencyTitle = json.base_currency_name;
+        //to currency full title
+        var toCurrencyTitle = json.rates[toCurrency].currency_name;
 
         //multiply users entered amount by currency rate to 
         var returnableAmount = currencyRate * fromAmount;
-        returnableAmount = returnableAmount.toFixed(2)
-
-        document.getElementById('finalValue').innerHTML = fromCurrency + ' ' + fromAmount + ' is equal to: ' + returnableAmount + ' ' + toCurrency;
+        //set the returned amount to 2 decimal places
+        returnableAmount = returnableAmount.toFixed(2);
+        //push the returned values to the dom
+        document.getElementById('finalValue').innerHTML = fromAmount + ' ' + fromCurrencyTitle + ' = ' + returnableAmount + ' ' + toCurrencyTitle;
+        document.getElementById('singleValue').innerHTML = '1 ' + fromCurrency + ' = ' + currencyRate + ' ' + toCurrency;
         }
     });
 
